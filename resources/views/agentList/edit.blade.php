6fc6f43144d372ef44539a294b3881dc9e4c40c5
‘name <18037297220@163.com>
2020-08-24 14:36:25 +0800

    去除注释的代码

--- resources/views/agentList/edit.blade.php
+++ resources/views/agentList/edit.blade.php
@@ -267,7 +267,7 @@
 @section('js')
     <script>
         layui.use(['form','jquery','layer'], function() {
-            var form = layui.form()
+            var form = layui.form
                 ,layer = layui.layer
                 ,$ = layui.jquery;
             form.render();
