c7528558d9cb97760cd64633ac7bf52385a07b1c
‘name <18037297220@163.com>
2020-08-24 14:34:19 +0800

    去除注释的代码

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,9 +18,13 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('admin/home');
+            return redirect('/admin/');
         }
 
         return $next($request);
+    }
+    protected function redirectTo()
+    {
+        return '/admin/';
     }
 }
