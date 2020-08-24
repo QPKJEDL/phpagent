b026a4f6f56b3518351e319da547dd77afad6f3f
‘name <18037297220@163.com>
2020-08-24 14:39:21 +0800

    去除注释的代码

--- resources/views/draw/list.blade.php
+++ resources/views/draw/list.blade.php
@@ -1,16 +1,23 @@
 @section('title', '会员列表')
 @section('header')
     <div class="layui-inline">
-        <button class="layui-btn layui-btn-small layui-btn-warm freshBtn"><i class="layui-icon">&#x1002;</i></button>
+        <button class="layui-btn layui-btn-small layui-btn-warm freshBtn"><i class="layui-icon">&#xe9aa;</i></button>
     </div>
     <div class="layui-inline">
-        <input class="layui-input" lay-verify="begin" name="begin" placeholder="开始时间" onclick="layui.laydate({elem: this, festival: true,min:'{{$min}}'})" value="{{ $input['begin'] or '' }}" autocomplete="off">
+        <input class="layui-input" lay-verify="begin" name="begin" placeholder="开始时间" id="begin" value="{{ $input['begin'] or '' }}" autocomplete="off">
     </div>
     <div class="layui-inline">
-        <input class="layui-input" lay-verify="end" name="end" placeholder="结束时间" onclick="layui.laydate({elem: this, festival: true,min:'{{$min}}'})" value="{{ $input['begin'] or '' }}" autocomplete="off">
+        <input class="layui-input" lay-verify="end" name="end" placeholder="结束时间" id="end" value="{{ $input['begin'] or '' }}" autocomplete="off">
     </div>
     <div class="layui-inline">
         <input type="text" lay-verify="account" value="{{ $input['account'] or '' }}" name="account" placeholder="用户账号" autocomplete="off" class="layui-input">
+    </div>
+    <div class="layui-inline">
+        <select name="status">
+            <option value="">请选择操作类型</option>
+            <option value="1" {{isset($input['status'])&&$input['status']==1?'selected':''}}>充值</option>
+            <option value="3" {{isset($input['status'])&&$input['status']==3?'selected':''}}>提现</option>
+        </select>
     </div>
     <div class="layui-inline">
         <button class="layui-btn layui-btn-normal" lay-submit lay-filter="formDemo">搜索</button>
@@ -19,7 +26,7 @@
     </div>
 @endsection
 @section('table')
-    <table class="layui-table" lay-even lay-skin="nob">
+    <table class="layui-table" lay-size="sm">
         <colgroup>
             <col class="hidden-xs" width="100">
             {{--<col class="hidden-xs" width="100">--}}
@@ -35,27 +42,43 @@
         <thead>
         <tr>
             <th class="hidden-xs">时间</th>
-            {{--<th class="hidden-xs">类型</th>--}}
             <th class="hidden-xs">用户名称[账号]</th>
-            <th class="hidden-xs">直属上级[账号]</th>
             <th class="hidden-xs">操作前金额</th>
             <th class="hidden-xs">充值提现金额</th>
             <th class="hidden-xs">操作后金额</th>
-            {{--<th class="hidden-xs">操作人</th>--}}
+            <th class="hidden-xs">操作类型</th>
+            <th class="hidden-xs">操作人</th>
         </tr>
         </thead>
         <tbody>
         @foreach($list as $info)
             <tr>
-                <td class="hidden-xs">{{$info['creatime']}}</td>
-                {{--<td class="hidden-xs"></td>--}}
-                <td class="hidden-xs">{{$info['nickname']}}[{{$info['account']}}]</td>
-                <td class="hidden-xs">{{$info['agentName']}}[{{$info['username']}}]</td>
-                {{--<td class="hidden-xs">{{$info['agent']['nickname']}}[{{$info['agent']['username']}}]</td>--}}
-                <td class="hidden-xs">{{$info['bet_before']/100}}</td>
-                <td class="hidden-xs">{{$info['money']/100}}</td>
-                <td class="hidden-xs">{{$info['bet_after']/100}}</td>
-                {{--<td class="hidden-xs"></td>--}}
+                <td class="hidden-xs">{{$info->creatime}}</td>
+                <td class="hidden-xs">{{$info->nickname}}[{{$info->account}}]</td>
+                <td class="hidden-xs">{{number_format($info->bet_before/100,2)}}</td>
+                <td class="hidden-xs">{{number_format($info->score/100,2)}}</td>
+                <td class="hidden-xs">{{number_format($info->bet_after/100,2)}}</td>
+                <td class="hidden-xs">
+                    @if($info->status==1)
+                        充值
+                    @elseif($info->status==3)
+                        提现
+                    @endif
+                    @if($info->pay_type==1)
+                        (到款)
+                    @elseif($info->pay_type==2)
+                        (签单)
+                    @elseif($info->pay_type==3)
+                        (移分)
+                    @elseif($info->pay_type==4)
+                        (按比例)
+                    @elseif($info->pay_type==5)
+                        (支付宝)
+                    @elseif($info->pay_type==6)
+                        (微信)
+                    @endif
+                </td>
+                <td class="hidden-xs">{{$info->remark}}</td>
             </tr>
         @endforeach
         @if(!$list[0])
@@ -64,18 +87,47 @@
         </tbody>
     </table>
     <div class="page-wrap">
-        {{$list->render()}}
+        <div id="demo"></div>
     </div>
 @endsection
 @section('js')
     <script>
-        layui.use(['form', 'jquery','laydate', 'layer'], function() {
-            var form = layui.form(),
+        layui.use(['laypage','form', 'jquery','laydate', 'layer'], function() {
+            var form = layui.form,
                 $ = layui.jquery,
                 laydate = layui.laydate,
-                layer = layui.layer
+                layer = layui.layer,
+                laypage = layui.laypage
             ;
-            laydate({istoday: true});
+            var count = {{$list->total()}};
+            var curr = {{$list->currentPage()}};
+            var limit = {{$limit}};
+            var url = "";
+            //分页
+            laypage.render({
+                elem: 'demo'
+                ,count: count
+                ,curr:curr
+                ,limit:limit
+                ,limits:[10,50,100,150]
+                ,layout: ['count', 'prev', 'page', 'next', 'limit', 'refresh', 'skip']
+                ,jump: function(obj,first){
+                    if(url.indexOf("?") >= 0){
+                        url = url.split("?")[0] + "?page=" + obj.curr + "&limit="+ obj.limit + "&" +$("form").serialize();
+                    }else{
+                        url = url + "?page=" + obj.curr + "&limit="+obj.limit;
+                    }
+                    if (!first){
+                        location.href = url;
+                    }
+                }
+            });
+            laydate.render({
+                elem:"#begin"
+            });
+            laydate.render({
+                elem:"#end"
+            });
             $(".reset").click(function(){
                 $("input[name='begin']").val('');
                 $("input[name='end']").val('');
