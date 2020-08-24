f1c8130ee2135bb1811356690eaa097b661d99a1
‘name <18037297220@163.com>
2020-08-24 14:33:45 +0800

    去除注释的代码

--- app/Http/Controllers/Online/OnHqUserController.php
+++ app/Http/Controllers/Online/OnHqUserController.php
@@ -15,23 +15,31 @@ class OnHqUserController extends Controller
     public function index(Request $request)
     {
         $map = array();
-        $map['agent_id']=Auth::id();
+        $map['user.agent_id']=Auth::id();
         if(true==$request->has('account')){
-            $map['account']=$request->input('account');
+            $map['user.account']=HttpFilter($request->input('account'));
         }
         $user = HqUser::query();
         $sql = $user->leftJoin('agent_users','user.agent_id','=','agent_users.id')
             ->leftJoin('user_account','user.user_id','=','user_account.user_id')
             ->select('user.*','agent_users.nickname as agentName','user_account.balance')->where($map);
         if(true ==$request->has('nickname')){
-            $sql->where('user.nickname','like','%'.$request->input('nickname').'%');
+            $sql->where('user.nickname','like','%'.HttpFilter($request->input('nickname')).'%');
         }
-        $data = $sql->paginate(10)->appends($request->all());
+        if (true==$request->has('limit'))
+        {
+            $limit = (int)$request->input('limit');
+        }
+        else
+        {
+            $limit = 10;
+        }
+        $data = $sql->paginate($limit)->appends($request->all());
         foreach($data as $key=>$value){
             $data[$key]['cz']=$this->getUserCzCord($value['user_id']);
             $data[$key]['creatime']=date('Y-m-d H:i:s',$value['creatime']);
         }
-        return view('onAgent.agentList.userList',['list'=>$data,'input'=>$request->all()]);
+        return view('onAgent.agentList.userList',['list'=>$data,'input'=>$request->all(),'limit'=>$limit]);
     }
 
     /**
