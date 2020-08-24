561261fd56d76eaeab1a76c38f7ddfee629d22f5
‘name <18037297220@163.com>
2020-08-24 14:27:21 +0800

    去除注释的代码

--- app/Http/Controllers/Admin/DrawController.php
+++ app/Http/Controllers/Admin/DrawController.php
@@ -2,56 +2,107 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Billflow;
 use App\Models\Draw;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class DrawController extends Controller
 {
     public function index(Request $request){
         $map = array();
-        $draw = Draw::query();
-        $sql = $draw->leftJoin('user','user.user_id','=','user_draw.user_id')
-            ->leftJoin('agent_users','agent_users.id','=','user.agent_id')
-            ->select('user_draw.creatime','user.nickname','user.account','agent_users.username','agent_users.nickname as agentName','user_draw.bet_before','user_draw.money','user_draw.bet_after')->where($map);
-        if (true==$request->has('begin')){
-            $begin = strtotime($request->input('begin'));
-            if (true==$request->has('end')){
-                $end = strtotime('+1day',strtotime($request->input('end')))-1;
-            }else{
-                $end = strtotime('+1day',time())-1;
-            }
-            $sql->whereBetween('user_draw.creatime',[$begin,$end]);
+        if (true==$request->has('begin'))
+        {
+            $startDate = $request->input('begin');
         }
-        if (true==$request->has('account')){
-            $sql->where('user.account','=',$request->input('account'));
+        else
+        {
+            $startDate = date('Y-m-d',time());
+            $request->offsetSet('begin',date('Y-m-d',time()));
         }
-        $sql->orderBy('user_draw.creatime','desc');
-        if (true==$request->input('excel') && true==$request->has('excel')){
-            $excel = $sql->get()->toArray();
-            $head = array('时间','用户名称[账号]','直属上级[账号]','操作前金额(元)','提现金额(元)','操作后金额(元)');
-            foreach ($excel as $key=>$value){
-                $excel[$key]['creatime']=date('Y-m-d H:i:s',$value['creatime']);
-                $excel[$key]['nickname']=$value['nickname'].'['.$value['account'].']';
-                $excel[$key]['agentName']=$value['agentName'].'['.$value['username'].']';
-                $excel[$key]['bet_before']=$value['bet_ before']/100;
-                $excel[$key]['money']=$value['money']/100;
-                $excel[$key]['bet_after']=$value['bet_after']/100;
-                unset($excel[$key]['account']);
-                unset($excel[$key]['username']);
-            }
-            try {
-                exportExcel($head, $excel, '会员提现记录' . date('YmdHis', time()), '', true);
-            } catch (\PHPExcel_Reader_Exception $e) {
-            } catch (\PHPExcel_Exception $e) {
-            }
-        }else{
-            $data = $sql->paginate(10)->appends($request->all());
-            foreach($data as $key=>&$value){
-                $data[$key]['creatime']=date('Y-m-d H:i:s',$value['creatime']);
-            }
+        if (true==$request->has('end'))
+        {
+            $endDate = $request->input('end');
         }
-        return view('draw.list',['list'=>$data,'input'=>$request->all(),'min'=>config('admin.min_date')]);
+        else
+        {
+            $endDate = date('Y-m-d',time());
+            $request->offsetSet('end',date('Y-m-d',time()));
+        }
+        if (true==$request->has('account'))
+        {
+            $map['user.account']=HttpFilter($request->input('account'));
+        }
+        if (true==$request->has('user_type'))
+        {
+            $map['user.user_type']=(int)$request->input('user_type');
+        }
+        $dateArr = $this->getDateTimePeriodByBeginAndEnd($startDate,$endDate);
+        //保存类型
+        $status = array();
+        if (true==$request->has('status'))
+        {
+            $status[]=(int)$request->input('status');
+        }
+        else
+        {
+            $status[] = 1;
+            $status[]=3;
+        }
+        //获取本人包括本人以下的所有代理id
+        $idArr = array();
+        $idArr[] = Auth::id();
+        $agentIdData = User::whereRaw('FIND_IN_SET('.Auth::id().',ancestors)',true)->select('id')->get();
+        foreach ($agentIdData as $key=>$datum)
+        {
+            $idArr[]=$datum['id'];
+        }
+        //获取第一天的数据
+        $bill = new Billflow();
+        $bill->setTable('user_billflow_'.$dateArr[0]);
+        $sql = $bill->leftJoin('user','user_billflow_'.$dateArr[0].'.user_id','=','user.user_id')
+            ->select('user_billflow_'.$dateArr[0].'.*','user.account','user.nickname','user.agent_id')->whereIn('user.agent_id',$idArr)->where($map)->whereIn('user_billflow_'.$dateArr[0].'.status',$status);
+        for ($i=1;$i<count($dateArr);$i++)
+        {
+            $b = new Billflow();
+            $b->setTable('user_billflow_'.$dateArr[$i]);
+            $d = $b->leftJoin('user','user_billflow_'.$dateArr[$i].'.user_id','=','user.user_id')
+                ->select('user_billflow_'.$dateArr[$i].'.*','user.account','user.nickname','user.agent_id')->whereIn('user.agent_id',$idArr)->where($map)->whereIn('user_billflow_'.$dateArr[$i].'.status',$status);
+            $sql->unionAll($d);
+        }
+        if (true==$request->has('limit'))
+        {
+            $limit = (int)$request->input('limit');
+        }
+        else
+        {
+            $limit = 10;
+        }
+        $data = DB::table(DB::raw("({$sql->toSql()}) as a"))->mergeBindings($sql->getQuery())->paginate($limit)->appends($request->all());
+        foreach ($data as $key=>$datum)
+        {
+            $data[$key]->creatime=date('Y-m-d H:i:s',time());
+        }
+        return view('draw.list',['limit'=>$limit,'list'=>$data,'input'=>$request->all()]);
+    }
+
+
+    /**
+     * 根据开始时间结束时间获取中间得时间段
+     * @param $startDate
+     * @param $endDate
+     * @return array
+     */
+    public function getDateTimePeriodByBeginAndEnd($startDate,$endDate){
+        $arr = array();
+        $start_date = date("Y-m-d",strtotime($startDate));
+        $end_date = date("Y-m-d",strtotime($endDate));
+        for ($i = strtotime($start_date); $i <= strtotime($end_date);$i += 86400){
+            $arr[] = date('Ymd',$i);
+        }
+        return $arr;
     }
 
     /**
