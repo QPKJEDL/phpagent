95b18572922afe717386b5f36d94c754f56bcd93
‘name <18037297220@163.com>
2020-08-24 14:38:27 +0800

    去除注释的代码

---
+++ resources/views/delAgent/list.blade.php
@@ -0,0 +1,103 @@
+@section('title', '会员列表')
+@section('header')
+    <div class="layui-inline">
+        <button class="layui-btn layui-btn-small layui-btn-warm freshBtn"><i class="layui-icon">&#xe9aa;</i></button>
+    </div>
+    <div class="layui-inline">
+        <input type="text" lay-verify="username" value="{{ $input['username'] or '' }}" name="username" placeholder="用户账号" autocomplete="off" class="layui-input">
+    </div>
+    <div class="layui-inline">
+        <input type="text" lay-verify="nickname" value="{{ $input['nickname'] or '' }}" name="nickname" placeholder="昵称" autocomplete="off" class="layui-input">
+    </div>
+    <div class="layui-inline">
+        <button class="layui-btn layui-btn-normal" lay-submit lay-filter="formDemo">搜索</button>
+        <button class="layui-btn layui-btn-normal reset" lay-submit>重置</button>
+    </div>
+@endsection
+@section('table')
+    <table class="layui-table" lay-size="sm">
+        <colgroup>
+            <col class="hidden-xs" width="100">
+            <col class="hidden-xs" width="100">
+            <col class="hidden-xs" width="100">
+            <col class="hidden-xs" width="100">
+            <col class="hidden-xs" width="100">
+            <col class="hidden-xs" width="100">
+            <col class="hidden-xs" width="100">
+        </colgroup>
+        <thead>
+        <tr>
+            <th class="hidden-xs">昵称</th>
+            <th class="hidden-xs">代理账号</th>
+            <th class="hidden-xs">最近充值</th>
+            <th class="hidden-xs">账户余额</th>
+            <th class="hidden-xs">洗码率</th>
+            <th class="hidden-xs">占城</th>
+            <th class="hidden-xs">创建日期</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($list as $info)
+            <tr>
+                <td class="hidden-xs">{{$info['nickname']}}</td>
+                <td class="hidden-xs">{{$info['username']}}</td>
+                <td class="hidden-xs">0.00</td>
+                <td class="hidden-xs">{{number_format($info['balance']/100,2)}}</td>
+                <td class="hidden-xs">{{$info['fee']['baccarat']}}/{{$info['fee']['dragonTiger']}}/{{$info['fee']['niuniu']}}/{{$info['fee']['sangong']}}/{{$info['fee']['A89']}}</td>
+                <td class="hidden-xs">{{$info['proportion']}}%</td>
+                <td class="hidden-xs">{{$info['created_at']}}</td>
+            </tr>
+        @endforeach
+        @if(!$list[0])
+            <tr><td colspan="7" style="text-align: center;color: orangered;">暂无数据</td></tr>
+        @endif
+        </tbody>
+    </table>
+    <div class="page-wrap">
+        <div id="demo"></div>
+    </div>
+@endsection
+@section('js')
+    <script>
+        layui.use(['form', 'jquery','laydate', 'layer','laypage'], function() {
+            var form = layui.form,
+                $ = layui.jquery,
+                laydate = layui.laydate,
+                layer = layui.layer,
+                laypage = layui.laypage
+            ;
+            var count = {{$list->total()}};
+            var curr = {{$list->currentPage()}};
+            var limit = {{$limit}};
+            var url = "";
+            //分页加载
+            laypage.render({
+                elem: 'demo'
+                ,count: count
+                ,curr:curr
+                ,limit:limit
+                ,limits:[10,50,100,150]
+                ,layout: ['count', 'prev', 'page', 'next', 'limit', 'refresh', 'skip']
+                ,jump: function(obj,first){
+                    if(url.indexOf("?") >= 0){
+                        url = url.split("?")[0] + "?page=" + obj.curr + "&limit="+ obj.limit + "&" +$("form").serialize();
+                    }else{
+                        url = url + "?page=" + obj.curr + "&limit="+obj.limit;
+                    }
+                    if (!first){
+                        location.href = url;
+                    }
+                }
+            });
+            $(".reset").click(function(){
+                $("input[name='account']").val('');
+                $("input[name='nickname']").val('');
+            });
+            form.render();
+            form.on('submit(formDemo)', function(data) {
+                console.log(data);
+            });
+        });
+    </script>
+@endsection
+@extends('common.list')
