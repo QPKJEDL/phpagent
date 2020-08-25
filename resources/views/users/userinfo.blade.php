3e85f6f804752ccb1f7c9e9da2cebf496c94c941
‘name <18037297220@163.com>
2020-08-25 11:32:15 +0800

    去除注释的代码

--- resources/views/users/userinfo.blade.php
+++ resources/views/users/userinfo.blade.php
@@ -12,54 +12,10 @@
 <body>
 <div class="layui-tab page-content-wrap">
     <ul class="layui-tab-title">
-        <li class="layui-this">修改资料</li>
-        <li>修改密码</li>
+        <li class="layui-this">修改密码</li>
     </ul>
     <div class="layui-tab-content">
         <div class="layui-tab-item layui-show">
-            <form class="layui-form"  style="width: 90%;padding-top: 20px;" id="info_form">
-                {{ csrf_field() }}
-                <div class="layui-form-item">
-                    <label class="layui-form-label">用户名：</label>
-                    <div class="layui-input-block">
-                        <input type="text" name="username" disabled autocomplete="off" class="layui-input layui-disabled" value="{{$userinfo['username']}}">
-                    </div>
-                </div>
-                <div class="layui-form-item">
-                    <label class="layui-form-label">邮箱：</label>
-                    <div class="layui-input-block">
-                        <input type="text" name="useremail" required  lay-verify="required|email" placeholder="请输入标题" autocomplete="off" class="layui-input" value="{{$userinfo['email']}}">
-                    </div>
-                </div>
-                <div class="layui-form-item">
-                    <label class="layui-form-label">手机号：</label>
-                    <div class="layui-input-block">
-                        <input type="text" name="usertel" required  lay-verify="required|phone" placeholder="请输入标题" autocomplete="off" class="layui-input" value="{{$userinfo['mobile']}}">
-                    </div>
-                </div>
-                <div class="layui-form-item">
-                    <label class="layui-form-label">性别：</label>
-                    <div class="layui-input-block">
-                        <input type="radio" name="usersex" value="1" title="男"
-                               @if(!isset($userinfo['sex']))
-                               checked
-                               @elseif(isset($userinfo['sex'])&&$userinfo['sex'])
-                               checked
-                                @else
-                                @endif>
-                        <input type="radio" name="usersex" value="0" title="女" {{isset($userinfo['sex'])&&!$userinfo['sex']?'checked':''}}>
-                    </div>
-
-                </div>
-                <input name="id" type="hidden" value="{{$userinfo['id'] or 0}}">
-                <div class="layui-form-item">
-                    <div class="layui-input-block">
-                        <button class="layui-btn layui-btn-normal" lay-submit lay-filter="adminInfo">立即提交</button>
-                    </div>
-                </div>
-            </form>
-        </div>
-        <div class="layui-tab-item">
             <form class="layui-form" style="width: 90%;padding-top: 20px;" id="pwd_form">
                 {{ csrf_field() }}
                 <div class="layui-form-item">
@@ -105,16 +61,16 @@
         form.render();
         form.verify({
             oldpwd:function(value){
-                if(value&&!/^(?!([a-zA-Z]+|\d+)$)[a-zA-Z\d]{6,12}$/.test(value)){
-                    return '旧密码必须6到12位数字加字母';
+                if(value.length<6){
+                    return '旧密码长度不能低于6位';
                 }
             },
             pwd:function(value){
                 if(value==$("input[name='oldpwd']").val()){
                     return '新密码不能与旧密码一样';
                 }
-                if(value&&!/^(?!([a-zA-Z]+|\d+)$)[a-zA-Z\d]{6,12}$/.test(value)){
-                    return '旧密码必须6到12位数字加字母';
+                if(value.length<6){
+                    return '旧密码长度不能低于6位';
                 }
             },
             pwd_confirmation: function(value) {
@@ -123,30 +79,9 @@
                 }
             },
         });
-        form.on('submit(adminInfo)', function(data){
-            $.ajax({
-                url:"{{url('/admin/saveinfo/1')}}",
-                data:$('#info_form').serialize(),
-                type:'post',
-                dataType:'json',
-                success:function(res){
-                    if(res.status == 1){
-                        layer.msg(res.msg,{icon:6});
-                        var index = parent.layer.getFrameIndex(window.name);
-                        setTimeout('parent.layer.close('+index+')',2000);
-                    }else{
-                        layer.msg(res.msg,{shift: 6,icon:5});
-                    }
-                },
-                error : function(XMLHttpRequest, textStatus, errorThrown) {
-                    layer.msg('网络失败', {time: 1000});
-                }
-            });
-            return false;
-        });
         form.on('submit(adminPassword)', function(data){
             $.ajax({
-                url:"{{url('/admin/saveinfo/2')}}",
+                url:"{{url('/admin/updatePwd')}}",
                 data:$('#pwd_form').serialize(),
                 type:'post',
                 dataType:'json',
