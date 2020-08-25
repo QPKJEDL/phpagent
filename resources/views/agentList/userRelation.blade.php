8ceb29b636fe21d24fa531e9e16f8d62f2fd3da3
‘name <18037297220@163.com>
2020-08-25 20:19:54 +0800

    去除注释的代码

--- resources/views/agentList/userRelation.blade.php
+++ resources/views/agentList/userRelation.blade.php
@@ -3,23 +3,43 @@
     <fieldset class="layui-elem-field layui-field-title" style="margin-top: 20px;">
         <legend>{{$info['account']}}结构关系</legend>
     </fieldset>
-    {{--赤色分割线
-    <hr class="layui-bg-red">--}}
-    @foreach($parent as $p)
-        {{$p['username']}}({{$p['nickname']}}) 创建日期：{{$p['created_at']}}
-        <hr class="layui-bg-orange">
-    @endforeach
-    {{$info['account']}}({{$info['nickname']}}) 创建日期：{{$info['creatime']}}
-    <hr class="layui-bg-orange">
+    <ul class="layui-timeline">
+        <li class="layui-timeline-item">
+            <i class="layui-icon layui-timeline-axis"></i>
+            <div class="layui-timeline-content layui-text">
+                <h3 class="layui-timeline-title">admin(总公司)</h3>
+            </div>
+        </li>
+        @foreach($parent as $p)
+            <li class="layui-timeline-item">
+                <i class="layui-icon layui-timeline-axis"></i>
+                <div class="layui-timeline-content layui-text">
+                    <h3 class="layui-timeline-title">{{$p['username']}}({{$p['nickname']}})</h3>
+                    <p>
+                        @if($p['userType']==2)[抽水{{$p['pump']}}%]@endif[占成{{$p['proportion']}}%]<br/>
+                        创建日期：{{$p['created_at']}}
+                    </p>
+                </div>
+            </li>
+        @endforeach
+        <li class="layui-timeline-item">
+            <i class="layui-icon layui-timeline-axis"></i>
+            <div class="layui-timeline-content layui-text">
+                <h3 class="layui-timeline-title">{{$info['account']}}({{$info['nickname']}})</h3>
+                <p>
+                    创建日期：{{$info['creatime']}}
+                </p>
+            </div>
+        </li>
+    </ul>
 @endsection
 @section('id','')
 @section('js')
     <script>
         layui.use(['form','jquery','laypage', 'layer','tree'], function() {
             var tree = layui.tree;
-            var form = layui.form(),
+            var form = layui.form,
                 $ = layui.jquery;
-            form.render();
             var layer = layui.layer;
             form.verify({
             });
--- resources/views/common/edita.blade.php
+++ resources/views/common/edita.blade.php
@@ -6,11 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
     <title>@yield('title') | {{ Config::get('app.name') }}</title>
-    <link rel="stylesheet" type="text/css" href="/static/admin/layui/css/layui.css" />
+    <link rel="stylesheet" type="text/css" href="/static/tools/layui/css/layui.css" />
     <link rel="stylesheet" type="text/css" href="/static/admin/css/admin.css" />
     <link href="/static/admin/summernote/summernote.css" rel="stylesheet">
     <link href="/static/admin/summernote/summernote-bs3.css" rel="stylesheet">
-    <script src="/static/admin/layui/layui.js" type="text/javascript" charset="utf-8"></script>
+    <script src="/static/tools/layui/layui.js" type="text/javascript" charset="utf-8"></script>
     <script src="/static/admin/js/common.js" type="text/javascript" charset="utf-8"></script>
     <script src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script>
